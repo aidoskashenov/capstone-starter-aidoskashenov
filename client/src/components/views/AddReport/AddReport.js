ef7e431b74364f3d51f15f3eb0c73d69cdbe0246
Aidos <aidos_ka@yahoo.com>
2020-09-12 14:57:59 -0500

    Add radio

--- client/src/components/views/AddReport/AddReport.js
+++ client/src/components/views/AddReport/AddReport.js
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 import api from "api";
 
 export const AddReport = () => {
+  const [rw, setRw] = useState("05");
   const dailylogAPI = api("dailylog");
 
+  console.log(rw);
+
   // Dispatch 'init' to update all of the initial todos...if any
-return (
+  return (
     <Formik
       initialValues={{
         runway: "",
@@ -31,6 +34,7 @@ return (
         const dailylogAPI = api("dailylog");
         const dailylog = {
           date: new Date().toISOString(),
+          rw: rw,
           rw: {
             runway: values.runway,
             runwaystate: values.runwaystate,
@@ -49,7 +53,7 @@ return (
           militaryActivity: values.militaryActivity,
           comments: values.comments,
         };
-       const res = await dailylogAPI.create({dailylog})
+        const res = await dailylogAPI.create({ dailylog });
 
         // dailylogAPI.create()
         setSubmitting(false);
@@ -57,72 +61,81 @@ return (
     >
       <Form>
         <h1 className="title has-text-centered mt-4">Insert new Log</h1>
-        <div className="container has-text-left">
-          {/* <div className="control">
-              <label className="radio">
-                <Field type="radio" name="answer" />
-                05
-              </label>
-              <label className="radio">
-                <Field type="radio" name="answer" />
-                23
-              </label>
-            </div> */}
 
-          <label className="label">Runway Conditions</label>
-          <div className="field is-grouped">
-            <label className="label mr-2"> runway</label>
-            <div className="control">
-              <Field name="runway" className="input is-small" type="text" />
-            </div>
-            <label className="label"> runwaystate</label>
-            <div className="control">
-              <Field
-                name="runwaystate"
-                className="input is-small"
-                type="text"
-              />
-            </div>
-            <label className="label"> coverage</label>
-            <div className="control">
-              <Field name="coverage" className="input is-small" type="text" />
-            </div>
-            <label className="label"> breaking action</label>
-            <div className="control">
-              <Field
-                name="breakingAction"
-                className="input is-small"
-                type="text"
-              />
-            </div>
-          </div>
-          <label className="label">Weather ATIS</label>
-          <div className="control">
-            <Field name="weatherATIS" className="input is-small" type="text" />
-          </div>
-          <label className="label">Significant weather</label>
-          <div className="control">
-            <Field name="significantWeather" type="checkbox" />
-          </div>
+        <label className="label mt-3 mb-3 mr-3">Runway</label>
 
-          <div className="field is-grouped">
-            <label className="label mr-4">Equipment</label>
-            <label className="label mr-2">PSR</label>
-            <div className="control">
-              <Field name="psr" type="checkbox" />
-            </div>
-            <label className="label mr-2">SSR</label>
-            <div className="control">
-              <Field name="ssr" type="checkbox" />
-            </div>
-            <label className="label mr-2">ILS</label>
-            <div className="control">
-              <Field name="ils" type="checkbox" />
-            </div>
-            <label className="label mr-2">DME</label>
-            <div className="control">
-              <Field name="dme" type="checkbox" />
-            </div>
+        <div className="control">
+          <label className="radio">
+            <Field
+              type="radio"
+              label="05"
+              checked={rw === "05"}
+              value="05"
+              onClick={() => setRw("05")}
+            />
+            05
+          </label>
+          <label className="radio">
+            <Field
+              type="radio"
+              label="23"
+              checked={rw === "23"}
+              value="23"
+              onClick={() => setRw("23")}
+            />
+            23
+          </label>
+        </div>
+
+        <label className="label mt-3 mb-3">Runway Conditions</label>
+        <div className="field is-grouped">
+          <label className="label mr-2"> runway</label>
+          <div className="control">
+            <Field name="runway" className="input is-small" type="text" />
+          </div>
+          <label className="label mr-3"> runwaystate</label>
+          <div className="control">
+            <Field name="runwaystate" className="input is-small" type="text" />
+          </div>
+          <label className="label mr-3"> coverage</label>
+          <div className="control">
+            <Field name="coverage" className="input is-small" type="text" />
+          </div>
+          <label className="label mr-3"> breaking action</label>
+          <div className="control">
+            <Field
+              name="breakingAction"
+              className="input is-small"
+              type="text"
+            />
+          </div>
+        </div>
+        <label className="label">Weather ATIS</label>
+        <div className="control">
+          <Field name="weatherATIS" className="input is-small" type="text" />
+        </div>
+        <label className="label">Significant weather</label>
+        <div className="control">
+          <Field name="significantWeather" type="checkbox" />
+        </div>
+
+        <div className="field is-grouped">
+          <label className="label mr-4">Equipment</label>
+          <label className="label mr-2">PSR</label>
+          <div className="control">
+            <Field name="psr" type="checkbox" />
+          </div>
+          <label className="label mr-2">SSR</label>
+          <div className="control">
+            <Field name="ssr" type="checkbox" />
+          </div>
+          <label className="label mr-2">ILS</label>
+          <div className="control">
+            <Field name="ils" type="checkbox" />
+          </div>
+          <label className="label mr-2">DME</label>
+          <div className="control">
+            <Field name="dme" type="checkbox" />
           </div>
 
           <label className="label">Maintenance</label>
