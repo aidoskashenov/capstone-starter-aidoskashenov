de3c700db340c06dc8464dde1b6538341d6fdb26
Aidos <aidos_ka@yahoo.com>
2020-09-13 23:18:35 -0500

    Add uniq key

--- client/src/components/views/Main/ReportList.js
+++ client/src/components/views/Main/ReportList.js
@@ -1,35 +1,14 @@
 import React from "react";
 
 export const ReportList = ({ dailylog }) => {
-  // const renderDailylogs = (dailylog) => {
-  //   if (dailylog) {
-  //     dailylog.map((log) => {
-
-  //       return (
-  //         <tr>
-  //           <td width="5%">
-  //             <i className="fa fa-bell-o"></i>
-  //           </td>
-  //           <td>{log.date ? log.date : null}</td>
-  //           <td className="level-right">
-  //             <a className="button is-small is-primary" href="#">
-  //               View
-  //             </a>
-  //           </td>
-  //         </tr>
-  //       );
-  //     });
-  //   }
-  // };
-
   return (
     <div className="card-table">
       <div className="content">
         <table className="table is-fullwidth is-striped">
           <tbody>
             {dailylog
-              ? dailylog.map((item) => (
-                  <tr>
+              ? dailylog.map((item, i) => (
+                  <tr key={i}>
                     <td width="5%">
                       <i className="fa fa-bell-o"></i>
                     </td>
